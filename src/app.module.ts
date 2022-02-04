b1b0b2efd0b86f9a1f5c2feea27a1ebcaf61ccaf
Steve Lemus <stv8904@hotmail.com>
2022-02-04 11:56:24 -0600

    Env db protection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,15 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot();
 
 @Module({
   imports: [
     ProductModule,
     MongooseModule.forRoot(
-      'mongodb+srv://admin:admincafe@cluster0.gft1q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.gft1q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     ),
     UsersModule,
   ],
